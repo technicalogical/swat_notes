9d6e942ffc206abac5a16dec2e947b45be39ca4a
Brandon Lehman <lehman.brandon@gmail.com>
2018-12-11 02:18:28 -0700

    Packaged Application

---
+++ notes-win32-x64/resources/app/main.js
@@ -0,0 +1,37 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow(){
+  win= new BrowserWindow({width:400, height:600, icon:__dirname+'Swat-logo.png'});
+
+  win.setMenu(null);
+
+  win.loadURL(url.format({
+  pathname: path.join(__dirname, 'index.html'),
+  protocol: 'file:',
+  slashes: true
+  }));
+
+//Open devtools
+//win.webContents.openDevTools();
+
+win.on('closed', () => {
+  win = null;
+  });
+}
+
+
+app.on('ready', createWindow);
+
+
+
+  //Quit when all windows closed
+  app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+      app.quit();
+    }
+  });
+
